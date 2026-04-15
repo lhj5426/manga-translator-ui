fa28314bb58cafe1ba7911487b909a4ea954c17e
lhj5426 <30548000+lhj5426@users.noreply.github.com>
2026-04-15 18:49:04 +0800

    Add files via upload

---
+++ 检测是否能运行GPU.py
@@ -0,0 +1,10 @@
+﻿import torch
+
+print("PyTorch version:", torch.__version__)
+print("Is CUDA available:", torch.cuda.is_available())
+
+if torch.cuda.is_available():
+    print("Current CUDA device:", torch.cuda.current_device())
+    print("CUDA device name:", torch.cuda.get_device_name(torch.cuda.current_device()))
+else:
+    print("CUDA is not available, using CPU.")
